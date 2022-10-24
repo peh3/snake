7b4511a2445567f13ad935d9d4a851370630cae1
peh3 <34233025+peh3@users.noreply.github.com>
2022-10-24 16:05:59 +0800

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,8 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'http://192.168.108.129:9000',
+       serverUrl: 'http://192.168.56.101:9000',
+       token : "squ_2b013356574fbb46faeaac9aed6ae1a853f7f424",
        options : {
        'sonar.sources': '.',
-       //'sonar.inclusions' : '.' // Entry point of your code
+       'sonar.projectName': 'sonar-project',
+       'sonar.projectDescription': 'Description for "My App" project...',
+       //'sonar.sources': 'src',
+       //'sonar.tests': 'specs'
        }
      }, () => {});
